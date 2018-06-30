3617758b74915c9fd69b91334e22bb0872e38e7f
Jorge A. Bernhardt <jorge.bernhardt@hotmail.com>
2018-07-01 00:13:17 +0200

    v0.1

---
+++ Rename-Computers-CSV.ps1
@@ -0,0 +1,22 @@
+﻿# Rename-Computers-CSV.ps1
+# v0.1
+# wwww.jorgebernhardt.com
+
+$File = 'c:\script\ComputerList.csv'
+$Credential=Get-Credential
+
+
+$computerList= Import-Csv   -Path $File `
+                            -Delimiter "," `
+                            -Header OldName,NewName
+
+foreach ($Computer in $computerList)
+{
+Rename-Computer -ComputerName $Computer.OldName `
+                -NewName $Computer.NewName `
+                -LocalCredential $Credential `
+                -Force `
+                -Restart           
+}
+
+
