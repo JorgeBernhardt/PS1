ded3eb8f8a40ffb87f29e0da5c1032809aea1301
Jorge A. Bernhardt <jorge.bernhardt@hotmail.com>
2019-07-31 14:24:24 +0200

    Update New-StoragePool.ps1

--- How to create a Storage Pool using PowerShell/New-StoragePool.ps1
+++ How to create a Storage Pool using PowerShell/New-StoragePool.ps1
@@ -1,3 +1,7 @@
+# Rename-Computers-CSV.ps1
+# v0.1
+# wwww.jorgebernhardt.com
+
 $disks = Get-PhysicalDisk -CanPool $true
 New-StoragePool -FriendlyName StoragePool-01 `
                 -StorageSubSystemFriendlyName "Storage Spaces on vDC01-N" `
